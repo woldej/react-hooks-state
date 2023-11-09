fefd9353dbbb679870c535d960999a9c5027bc3e
woldejamawolde <wolde.jama@m0ringaschool.com>
2023-11-09 21:33:54 +0300

    completed assignment

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,7 +4,9 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
-    setCount(count + 1);
+    setCount((currentCount) => currentCount + 1);
+  setCount((currentCount) => currentCount + 1);
+  console.log(count);
   }
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
